8d055f10d3a9ea50435902b1184b013fcc894c3e
大石头 <nnhy@vip.qq.com>
2022-12-18 23:15:38 +0800

    新增C++项目，静态编译输出可以在winxpsp2上运行

---
+++ Clover/Clover.cpp
@@ -0,0 +1,25 @@
+﻿#include <windows.h>
+#include <stdio.h>
+
+void main()
+{
+    OSVERSIONINFO osvi;
+    BOOL bIsWindowsXPorLater;
+
+    ZeroMemory(&osvi, sizeof(OSVERSIONINFO));
+    osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+
+    GetVersionEx(&osvi);
+
+    printf("osver: %d.%d.%d\r\n", osvi.dwMajorVersion, osvi.dwMinorVersion, osvi.dwBuildNumber);
+    printf("PlatformId: %d\r\n", osvi.dwPlatformId);
+
+    //bIsWindowsXPorLater =
+    //    ((osvi.dwMajorVersion > 5) ||
+    //        ((osvi.dwMajorVersion == 5) && (osvi.dwMinorVersion >= 1)));
+
+    //if (bIsWindowsXPorLater)
+    //    printf("The system meets the requirements.\n");
+    //else 
+    //    printf("The system does not meet the requirements.\n");
+}
---
+++ Clover/stdafx.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <SDKDDKVer.h>
+
+#include <stdio.h>
+#include <tchar.h>
