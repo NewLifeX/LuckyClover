0d7dc578960a5361a91b5ba414ebb3ba6bea3f20
大石头 <nnhy@vip.qq.com>
2022-12-19 00:27:20 +0800

    访问注册表；成功下载文件并保存到本地

--- Clover/Clover.cpp
+++ Clover/Clover.cpp
@@ -4,6 +4,9 @@
 #include "httplib.h"
 
 #include <windows.h>
+#pragma comment (lib,"Advapi32.lib")
+
+using namespace std;
 
 void main()
 {
@@ -18,6 +21,22 @@ void main()
     printf("osver: %d.%d.%d\r\n", osvi.dwMajorVersion, osvi.dwMinorVersion, osvi.dwBuildNumber);
     printf("PlatformId: %d\r\n", osvi.dwPlatformId);
 
+    string releaseId = "";
+    HKEY hKey_return = NULL;
+    char keyValue[256];
+    DWORD keySzType;
+    DWORD keySize;
+
+    if (ERROR_SUCCESS != RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\", 0, KEY_READ, &hKey_return)) {
+        printf("RegOpenKeyEx failed.\n");;
+    }
+    else if (ERROR_SUCCESS != RegQueryValueEx(hKey_return, "Version", 0, &keySzType, (LPBYTE)&keyValue, &keySize)) {
+        printf("RegQueryValueEx failed.\n");
+    }
+    else {
+        printf("ReleaseId: %s\n", keyValue);
+    }
+
     //// HTTP
     //httplib::Client cli("http://star.newlifex.com:6600");
 
@@ -30,12 +49,26 @@ void main()
     // HTTP
     httplib::Client cli("http://x.newlifex.com");
 
-    auto res = cli.Get("/dotnet/dotNetFx40_Full_x86_x64.exe",
-        [&](const char* data, size_t data_length) {
-            //body.append(data, data_length);
-            printf("%d", data_length);
-            return true;
-        });
+    if (auto res = cli.Get("/dotnet/dotNetFx40_Full_x86_x64.exe")) {
+        printf("status:%d\n", res->status);
+        if (res->status == 200)
+        {
+            std::ofstream out;
+            out.open("dotNetFx40_Full_x86_x64.exe", std::ios_base::binary | std::ios::out);
+            printf("savefile!\n");
+            if (out.is_open())
+            {
+                out << res->body;
+                out.flush();
+                out.close();
+                printf("down load file finished!\n");
+            }
+            else
+            {
+                printf("open file error!\n");
+            }
+        }
+    }
 
     //// HTTP
     //httplib::Client cli("http://x.newlifex.com");
