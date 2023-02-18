eb6f3031deb0992e463d33adf1a13dd0b09d08f0
大石头 <nnhy@vip.qq.com>
2023-02-18 22:39:35 +0800

    增强zip功能，支持压缩多个目录的多文件

--- LuckyClover/Program.cs
+++ LuckyClover/Program.cs
@@ -183,16 +183,98 @@ internal class Program
     {
         if (args == null || args.Length < 3) return;
 
-        var src = args[1];
-        var dst = args[2];
+        if (args.Length == 3)
+        {
+            var dst = args[1];
+            var src = args[2];
 
-        Console.WriteLine("Zip压缩 {0} 到 {1}", src, dst);
+            Console.WriteLine("Zip压缩 {0} 到 {1}", src, dst);
 
 #if NET7_0_OR_GREATER
-        ZipFile.CreateFromDirectory(src, dst, CompressionLevel.SmallestSize, false);
+            ZipFile.CreateFromDirectory(src, dst, CompressionLevel.SmallestSize, false);
 #else
-        ZipFile.CreateFromDirectory(src, dst, CompressionLevel.Optimal, false);
+            ZipFile.CreateFromDirectory(src, dst, CompressionLevel.Optimal, false);
 #endif
+        }
+        else
+        {
+#if NET7_0_OR_GREATER
+            var dst = args[1];
+
+            Console.WriteLine("Zip压缩多个文件到 {0}", dst);
+
+            if (File.Exists(dst)) File.Delete(dst);
+
+            var compressionLevel = CompressionLevel.SmallestSize;
+            using var zip = ZipFile.Open(dst, ZipArchiveMode.Create);
+
+            // 遍历多个目录或文件
+            for (var i = 2; i < args.Length; i++)
+            {
+                // 分离路径中的目录和文件掩码
+                var src = args[i];
+                var pt = "*";
+                var p = src.LastIndexOfAny(new[] { '/', '\\' });
+                if (p > 0)
+                {
+                    pt = src.Substring(p + 1);
+                    src = src.Substring(0, p);
+                }
+
+                var di = new DirectoryInfo(src);
+                var fullName = di.FullName;
+                // 遍历所有文件
+                foreach (var fi in di.EnumerateFileSystemInfos(pt, SearchOption.AllDirectories))
+                {
+                    var length = fi.FullName.Length - fullName.Length;
+                    if (fi is FileInfo)
+                    {
+                        var entryName = EntryFromPath(fi.FullName, fullName.Length, length, false);
+                        zip.CreateEntryFromFile(fi.FullName, entryName, compressionLevel);
+                        continue;
+                    }
+                    if (fi is DirectoryInfo di2 && IsDirEmpty(di2))
+                    {
+                        var entryName2 = EntryFromPath(fi.FullName, fullName.Length, length, true);
+                        zip.CreateEntry(entryName2);
+                    }
+                }
+            }
+#endif
+        }
+    }
+
+    static String EntryFromPath(String entry, Int32 offset, Int32 length, Boolean appendPathSeparator = false)
+    {
+        while (length > 0 && (entry[offset] == Path.DirectorySeparatorChar || entry[offset] == Path.AltDirectorySeparatorChar))
+        {
+            offset++;
+            length--;
+        }
+        if (length == 0) return !appendPathSeparator ? String.Empty : "/";
+
+        var num = appendPathSeparator ? (length + 1) : length;
+        var buffer = new Char[num];
+        entry.CopyTo(offset, buffer, 0, length);
+        for (var i = 0; i < length; i++)
+        {
+            var c = buffer[i];
+            if (c == Path.DirectorySeparatorChar || c == Path.AltDirectorySeparatorChar)
+            {
+                buffer[i] = '/';
+            }
+        }
+        if (appendPathSeparator)
+        {
+            buffer[length] = '/';
+        }
+        return new String(buffer, 0, num);
+    }
+
+    static Boolean IsDirEmpty(DirectoryInfo possiblyEmptyDir)
+    {
+        using var enumerator = Directory.EnumerateFileSystemEntries(possiblyEmptyDir.FullName).GetEnumerator();
+        return !enumerator.MoveNext();
     }
 
     private static void Unzip(String[] args)
