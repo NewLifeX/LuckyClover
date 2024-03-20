69d895e5b46b0ce45802cec48623e020f30493c6
大石头 <nnhy@vip.qq.com>
2024-03-20 18:42:53 +0800

    [feat]接入星尘节点，产品名LuckClover

---
+++ Installer4/Setting.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using NewLife;
+using NewLife.Configuration;
+
+namespace Installer;
+
+[Config("Clover")]
+public class Setting : Config<Setting>
+{
+    #region 属性
+    /// <summary>证书</summary>
+    [Description("证书")]
+    public String Code { get; set; }
+
+    /// <summary>密钥</summary>
+    [Description("密钥")]
+    public String Secret { get; set; }
+
+    /// <summary>服务地址端口。默认为空，子网内自动发现</summary>
+    [Description("服务地址端口。默认为空，子网内自动发现")]
+    public String Server { get; set; } = "";
+    #endregion
+
+    #region 加载/保存
+    protected override void OnLoaded()
+    {
+        if (Server.IsNullOrEmpty()) Server = "http://s.newlifex.com:6600";
+
+        base.OnLoaded();
+    }
+    #endregion
+}
