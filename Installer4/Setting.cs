d0babb0ad37229056960440d2da09684e6ba6e01
大石头 <nnhy@vip.qq.com>
2024-07-13 10:35:44 +0800

    升级星尘，使用最新Remoting

---
+++ Installer4/Setting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using NewLife;
+using NewLife.Configuration;
+using NewLife.Remoting.Clients;
+
+namespace Installer;
+
+[Config("Clover")]
+public class Setting : Config<Setting>, IClientSetting
+{
+    #region 属性
+    /// <summary>证书</summary>
+    [Description("证书")]
+    public String Code { get; set; }
+
+    /// <summary>密钥</summary>
+    [Description("密钥")]
+    public String Secret { get; set; }
+
+    /// <summary>服务地址端口。默认为空，子网内自动发现</summary>
+    [Description("服务地址端口。默认为空，子网内自动发现")]
+    public String Server { get; set; } = "";
+    #endregion
+
+    #region 加载/保存
+    protected override void OnLoaded()
+    {
+        if (Server.IsNullOrEmpty()) Server = "http://s.newlifex.com:6600";
+
+        base.OnLoaded();
+    }
+    #endregion
+}
