394bea3cd4148139032a088a0385f9df8cb00fdf
大石头 <nnhy@vip.qq.com>
2025-07-07 16:47:36 +0800

    引入VC-LTL，摆脱微软运行时依赖，例如msvcr120.dll、api-ms-win-crt-time-l1-1-0.dll

--- Doc/buildAot.bat
+++ Doc/buildAot.bat
@@ -2,9 +2,9 @@ set base=..\Bin
 set dest=%base%\publish
 
 pushd ..\LuckyClover
-dotnet publish -c Release -f net9.0 -r win-x64 --self-contained
+dotnet publish -c Release -f net9.0-windows -r win-x64 --self-contained
 popd
 
 mkdir %dest%
 
-copy %base%\net9.0\win-x64\publish\clover.exe %dest%\clover90.exe /y
+copy %base%\net9.0-windows\win-x64\publish\clover.exe %dest%\clover90.exe /y
--- Doc/buildAot86.bat
+++ Doc/buildAot86.bat
@@ -2,9 +2,9 @@ set base=..\Bin
 set dest=%base%\publish
 
 pushd ..\LuckyClover
-dotnet publish -c Release -f net9.0 -r win-x86 --self-contained
+dotnet publish -c Release -f net9.0-windows -r win-x86 --self-contained
 popd
 
 mkdir %dest%
 
-copy %base%\net9.0\win-x86\publish\clover.exe %dest%\clover90-x86.exe /y
+copy %base%\net9.0-windows\win-x86\publish\clover.exe %dest%\clover90-x86.exe /y
